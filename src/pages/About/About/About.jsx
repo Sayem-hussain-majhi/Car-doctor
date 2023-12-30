63170b7f9580abebd148bcfdfa54d656fe101871
Sayem-hussain-majhi <hafejmdsayem@gmail.com>
2023-12-30 18:49:02 +0600

    [About Page] Done

--- src/pages/About/About/About.jsx
+++ src/pages/About/About/About.jsx
@@ -1,13 +1,20 @@
 import { Suspense, lazy } from "react";
+import OurTeam from "../../Home/OurTeam/OurTeam";
+import Address from "../../Home/Address/Address";
+import OurFacility from "../../Home/OurFacility/OurFacility";
 const Loading = lazy(()=> import("../../../Shared/Loading"))
+const About = lazy(() => import('../../Home/About/About'))
 
 
-const About = () => {
+const AboutPage = () => {
     return (
         <Suspense fallback={<Loading />}>
-            
+            <About />
+            <OurTeam />
+            <Address />
+            <OurFacility />
         </Suspense>
     );
 };
 
-export default About;
+export default AboutPage;
